4daa9e3f2c46f3d40f700f10b61d4d3da5a16b5f
Apipnhi <afif2605h@gmail.com>
2024-07-08 11:25:36 +0700

    first commit

---
+++ Feddback/src/Transibitor/UserSession.java
@@ -0,0 +1,17 @@
+package Transibitor;
+
+public class UserSession {
+    private static DataUser currentUser;
+
+    public static void login(DataUser user) {
+        currentUser = user;
+    }
+
+    public static void logout() {
+        currentUser = null;
+    }
+
+    public static DataUser getCurrentUser() {
+        return currentUser;
+    }
+}
